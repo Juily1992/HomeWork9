2685efb43a0e399ac71b993188b897d8280523e9
Juily1992 <juilliet1992@gmail.com>
2024-11-06 16:33:13 +0300

    ДЗ для проверки

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,84 @@
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.Collections;
+import java.io.FileInputStream;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + 1);
+    public static void main(String[] args) throws Exception {
+        // Task 1
+        System.out.println("Задача 1");
+        Scanner numberPayments = new Scanner(System.in);
+        System.out.println("Введите количество трат за месяц ");
+        int size = numberPayments.nextInt();
+        int payment[] = new int[size];
+        System.out.println("Введите суммы трат: ");
+        int sum = 0;
+        for (int i : payment) {
+            i++;
+            payment[i] = numberPayments.nextInt();
+            sum += payment[i];
         }
+        System.out.println("Cумма трат за месяц =  " + sum);
+
+
+        // Task 2
+        System.out.println("  ");
+        System.out.println("Задача 2");
+        Scanner minMaxWastes = new Scanner(System.in);
+        System.out.println("Введите количество трат за месяц ");
+        int numberWastes = minMaxWastes.nextInt();
+        int wastes[] = new int[numberWastes];
+        System.out.println("Введите суммы трат: ");
+        int max = wastes[0];
+        int min = wastes[0];
+        for (int i : wastes) {
+            wastes[i] = minMaxWastes.nextInt();
+            if (wastes[i] > max) {
+                max = wastes[i];
+
+            }
+            if (wastes[i] < max & wastes[i] != 0) {
+                min = wastes[i];
+            }
+        }
+        System.out.println("Максимальная выплата равна " + max);
+        System.out.println("Минимальная выплата равна " + min);
+
+
+        //Task 3
+        System.out.println("  ");
+        System.out.println("Задача 3");
+        Scanner numberSum = new Scanner(System.in);
+        System.out.println("Введите количество трат за месяц ");
+        int number = numberSum.nextInt();
+        int averageSum[] = new int[number];
+        System.out.println("Введите суммы трат: ");
+        int summ = 0;
+        for (int i : averageSum) {
+            averageSum[i] = numberSum.nextInt();
+            summ += averageSum[i];
+        }
+        System.out.println("Среднее арифметическое выплат равно " + (double) summ / averageSum.length);
+
+
+        //Task 4
+        System.out.println("  ");
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        System.out.print("Имя сотрудника - ");
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
     }
+
+
 }
+
